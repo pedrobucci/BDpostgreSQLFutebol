fec038856b6d09d9c713d8ede960d4273a5d1930
tuliolima <tuliook@hotmail.com>
2016-06-27 16:47:04 -0300

    Método pegar tabela

--- src/Dados/BancoDados.java
+++ src/Dados/BancoDados.java
@@ -8,50 +8,71 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BancoDados {
+	
+	Connection con = null;
+    PreparedStatement pst = null;
+    ResultSet rs = null;
 
-	public static void main(String[] args) {
-		
-		Connection con = null;
-        PreparedStatement pst = null;
-        ResultSet rs = null;
-
-        String url = "jdbc:postgresql://localhost/";
-        String user = "postgres";
-        String password = "tuliolima";
-
-        try {
-
-            String tabela = "tabela_de_jogos";
+    String url = "jdbc:postgresql://localhost/";
+    String user = "postgres";
+    String password = "tuliolima";
+    
+    public BancoDados () {
+    	
+    }
+    
+    private void conectar () {
+    	try {
             con = DriverManager.getConnection(url, user, password);
-
-            String stm = "select * from juiz;";
-            pst = con.prepareStatement(stm);
-            //pst.setString(1, tabela);                  
-            rs = pst.executeQuery();
-
-            while (rs.next()) {
-                System.out.print(rs.getString(2) + "\t");
-                System.out.println(rs.getString(4) + "\n");
+    	} catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(BancoDados.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+    
+    private void desconectar () {
+        try {
+            if (con != null) {
+                con.close();
+                con = null;
             }
-
         } catch (SQLException ex) {
             Logger lgr = Logger.getLogger(BancoDados.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
-
-        } finally {
-
-            try {
-                if (pst != null) {
-                    pst.close();
-                }
-                if (con != null) {
-                    con.close();
-                }
-
-            } catch (SQLException ex) {
-                Logger lgr = Logger.getLogger(BancoDados.class.getName());
-                lgr.log(Level.SEVERE, ex.getMessage(), ex);
-            }
         }
     }
+
+    public String getTabelaDeJogos () {
+    	String tabela = "";
+    	conectar();
+	    try {	
+	        String stm = "select * from tabela_de_jogos;";
+	        pst = con.prepareStatement(stm);
+	        //pst.setString(1, tabela);                  
+	        rs = pst.executeQuery();
+	
+	        while (rs.next()) {
+	            //System.out.print(rs.getString(2) + "\t");
+	            //System.out.println(rs.getString(4) + "\n");
+	            tabela += rs.getString(2) + "\t";
+	            tabela += rs.getString(4) + "\n";
+	        }
+	
+	    } catch (SQLException ex) {
+	        Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	        lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	
+	    } finally {
+	
+	        try {
+	            if (pst != null) {
+	                pst.close();
+	            }	
+	        } catch (SQLException ex) {
+	            Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	        }
+	    }
+	    return tabela;
+	}
 }
