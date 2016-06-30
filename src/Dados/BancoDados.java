1c0ca43ff3a12b0d938f1c6b4e520954c20697dd
tuliolima <tuliook@hotmail.com>
2016-06-30 13:01:57 -0300

    Programa finalizado

--- src/Dados/BancoDados.java
+++ src/Dados/BancoDados.java
@@ -41,6 +41,135 @@ public class BancoDados {
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
     }
+    
+    public String selectTabelaSelecao (String pais) {
+    	String tabela = "";
+    	String stm = "";
+    	conectar();
+	    try {	
+	    	if (pais.equals("")) {
+	    		stm = "select * from selecao;";
+	    	} else {
+	    		stm = "select * from selecao where pais = '" + pais + "'";
+	    	}
+	        pst = con.prepareStatement(stm);
+	        //pst.setString(1, tabela);                  
+	        rs = pst.executeQuery();
+	
+	        tabela += "|id|Pais          |Pontos|id_continente|id_centro_olimpico|\n"
+	        		+ "|--+--------------+------+-------------+------------------|\n";
+	        while (rs.next()) {
+	            tabela += String.format("|%2s|%-14s|%6d|%13s|%18s|\n", 
+	            		rs.getInt(1), 
+	            		rs.getString(2), 
+	            		rs.getInt(3),
+	            		rs.getInt(5),
+	            		rs.getInt(6));
+	        }
+	
+	    } catch (SQLException ex) {
+	        Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	        lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	
+	    } finally {
+	
+	        try {
+	            if (pst != null) {
+	                pst.close();
+	            }	
+	        } catch (SQLException ex) {
+	            Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	        }
+	    }
+	    return tabela;
+	}
+    
+    public String selectTabelaJogador (String nome) {
+    	String tabela = "";
+    	String stm = "";
+    	conectar();
+	    try {	
+	    	if (nome.equals("")) {
+	    		stm = "select * from jogador;";
+	    	} else {
+	    		stm = "select * from jogador where nome = '" + nome + "'";
+	    	}
+	        pst = con.prepareStatement(stm);
+	        //pst.setString(1, tabela);                  
+	        rs = pst.executeQuery();
+	
+	        tabela += "|id|Nome       |Idade|Nacionalidade|id_selecao|\n"
+	        		+ "|--+-----------+-----+-------------+----------|\n";
+	        while (rs.next()) {
+	            tabela += String.format("|%2s|%-11s|%5d|%13s|%10s|\n", 
+	            		rs.getInt(1), 
+	            		rs.getString(2), 
+	            		rs.getInt(3),
+	            		rs.getString(4),
+	            		rs.getInt(5));
+	        }
+	
+	    } catch (SQLException ex) {
+	        Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	        lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	
+	    } finally {
+	
+	        try {
+	            if (pst != null) {
+	                pst.close();
+	            }	
+	        } catch (SQLException ex) {
+	            Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	        }
+	    }
+	    return tabela;
+	}
+    
+    public String selectTabelaTecnico (String nome) {
+    	String tabela = "";
+    	String stm = "";
+    	conectar();
+	    try {	
+	    	if (nome.equals("")) {
+	    		stm = "select * from tecnico;";
+	    	} else {
+	    		stm = "select * from tecnico where nome = '" + nome + "'";
+	    	}
+	        pst = con.prepareStatement(stm);
+	        //pst.setString(1, tabela);                  
+	        rs = pst.executeQuery();
+	
+	        tabela += "|id|Nome            |Idade|Nacionalidade|id_selecao|\n"
+	        		+ "|--+----------------+-----+-------------+----------|\n";
+	        while (rs.next()) {
+	            tabela += String.format("|%2s|%-16s|%5d|%13s|%10s|\n", 
+	            		rs.getInt(1), 
+	            		rs.getString(2), 
+	            		rs.getInt(3),
+	            		rs.getString(4),
+	            		rs.getInt(5));
+	        }
+	
+	    } catch (SQLException ex) {
+	        Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	        lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	
+	    } finally {
+	
+	        try {
+	            if (pst != null) {
+	                pst.close();
+	            }	
+	        } catch (SQLException ex) {
+	            Logger lgr = Logger.getLogger(BancoDados.class.getName());
+	            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	        }
+	    }
+	    return tabela;
+	}
 
     public String getTabelaDeJogos () {
     	String tabela = "";
@@ -51,8 +180,10 @@ public class BancoDados {
 	        //pst.setString(1, tabela);                  
 	        rs = pst.executeQuery();
 	
+	        tabela += "|Rodada           |Selecao A     |   |Selecao B     |Local                 |Data/Hora             |\n"
+	        		+ "|-----------------+--------------+---+--------------+----------------------+----------------------|\n";
 	        while (rs.next()) {
-	            tabela += String.format("|%-17s|%14s|%1dx%1d|%-14s|%-22s|%-22s|\n", 
+	            tabela += String.format("|%-17s|%-14s|%1dx%1d|%-14s|%-22s|%-22s|\n", 
 	            		rs.getString(1), 
 	            		rs.getString(2), 
 	            		rs.getInt(3),
--- src/GUI/Interface.java
+++ src/GUI/Interface.java
@@ -79,7 +79,7 @@ public class Interface extends JFrame {
 		panel.add(textField);
 		textField.setColumns(10);
 		
-		textField_1 = new JTextField();
+		/*textField_1 = new JTextField();
 		textField_1.setBounds(106, 11, 86, 20);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
@@ -97,17 +97,24 @@ public class Interface extends JFrame {
 		textField_5 = new JTextField();
 		textField_5.setBounds(394, 11, 86, 20);
 		panel.add(textField_5);
-		textField_5.setColumns(10);
+		textField_5.setColumns(10);*/
 		
 		JTextPane textPane = new JTextPane();
 		textPane.setBounds(10, 48, 523, 188);
+		textPane.setFont(new Font("monospaced", Font.PLAIN, 12));
 		panel.add(textPane);
 		
 		JButton bBuscarSelecoes = new JButton("Buscar");
+		bBuscarSelecoes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textPane.setText(olimpiadas.selectTabelaSelecao(textField.getText()));
+			}
+		});
+		bBuscarSelecoes.setAction(action);
 		bBuscarSelecoes.setBounds(10, 247, 89, 23);
 		panel.add(bBuscarSelecoes);
 		
-		JButton bAdicionarSelecoes = new JButton("Adicionar");
+		/*JButton bAdicionarSelecoes = new JButton("Adicionar");
 		bAdicionarSelecoes.setBounds(106, 247, 89, 23);
 		panel.add(bAdicionarSelecoes);
 		
@@ -117,7 +124,7 @@ public class Interface extends JFrame {
 		
 		JButton bAtualizarSelecoes = new JButton("Atualizar");
 		bAtualizarSelecoes.setBounds(298, 247, 89, 23);
-		panel.add(bAtualizarSelecoes);
+		panel.add(bAtualizarSelecoes);*/
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setLayout(null);
@@ -128,7 +135,7 @@ public class Interface extends JFrame {
 		textField_3.setBounds(10, 11, 86, 20);
 		panel_1.add(textField_3);
 		
-		textField_6 = new JTextField();
+		/*textField_6 = new JTextField();
 		textField_6.setColumns(10);
 		textField_6.setBounds(106, 11, 86, 20);
 		panel_1.add(textField_6);
@@ -146,27 +153,33 @@ public class Interface extends JFrame {
 		textField_9 = new JTextField();
 		textField_9.setColumns(10);
 		textField_9.setBounds(394, 11, 86, 20);
-		panel_1.add(textField_9);
+		panel_1.add(textField_9);*/
 		
 		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setBounds(10, 48, 523, 188);
+		textPane_1.setBounds(10, 48, 523, 229);
+		textPane_1.setFont(new Font("monospaced", Font.PLAIN, 12));
 		panel_1.add(textPane_1);
 		
 		JButton bBuscarJogadores = new JButton("Buscar");
-		bBuscarJogadores.setBounds(10, 247, 89, 23);
+		bBuscarJogadores.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textPane_1.setText(olimpiadas.selectTabelaJogador(textField_3.getText()));
+			}
+		});
+		bBuscarJogadores.setBounds(10, 288, 89, 23);
 		panel_1.add(bBuscarJogadores);
 		
-		JButton bAdicionarJogadores = new JButton("Adicionar");
-		bAdicionarJogadores.setBounds(106, 247, 89, 23);
+		/*JButton bAdicionarJogadores = new JButton("Adicionar");
+		bAdicionarJogadores.setBounds(106, 288, 89, 23);
 		panel_1.add(bAdicionarJogadores);
 		
 		JButton bDeletarJogadores = new JButton("Deletar");
-		bDeletarJogadores.setBounds(202, 247, 89, 23);
+		bDeletarJogadores.setBounds(202, 288, 89, 23);
 		panel_1.add(bDeletarJogadores);
 		
 		JButton bAtualizarJogadores = new JButton("Atualizar");
-		bAtualizarJogadores.setBounds(298, 247, 89, 23);
-		panel_1.add(bAtualizarJogadores);
+		bAtualizarJogadores.setBounds(298, 288, 89, 23);
+		panel_1.add(bAtualizarJogadores);*/
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setLayout(null);
@@ -177,7 +190,7 @@ public class Interface extends JFrame {
 		textField_10.setBounds(10, 11, 86, 20);
 		panel_2.add(textField_10);
 		
-		textField_11 = new JTextField();
+		/*textField_11 = new JTextField();
 		textField_11.setColumns(10);
 		textField_11.setBounds(106, 11, 86, 20);
 		panel_2.add(textField_11);
@@ -195,17 +208,23 @@ public class Interface extends JFrame {
 		textField_14 = new JTextField();
 		textField_14.setColumns(10);
 		textField_14.setBounds(394, 11, 86, 20);
-		panel_2.add(textField_14);
+		panel_2.add(textField_14);*/
 		
 		JTextPane textPane_2 = new JTextPane();
 		textPane_2.setBounds(10, 48, 523, 188);
+		textPane_2.setFont(new Font("monospaced", Font.PLAIN, 12));
 		panel_2.add(textPane_2);
 		
 		JButton bBuscarTecnicos = new JButton("Buscar");
+		bBuscarTecnicos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textPane_2.setText(olimpiadas.selectTabelaTecnico(textField_10.getText()));
+			}
+		});
 		bBuscarTecnicos.setBounds(10, 247, 89, 23);
 		panel_2.add(bBuscarTecnicos);
 		
-		JButton bAdicionarTecnicos = new JButton("Adicionar");
+		/*JButton bAdicionarTecnicos = new JButton("Adicionar");
 		bAdicionarTecnicos.setBounds(106, 247, 89, 23);
 		panel_2.add(bAdicionarTecnicos);
 		
@@ -215,7 +234,7 @@ public class Interface extends JFrame {
 		
 		JButton bAtualizarTecnicos = new JButton("Atualizar");
 		bAtualizarTecnicos.setBounds(298, 247, 89, 23);
-		panel_2.add(bAtualizarTecnicos);
+		panel_2.add(bAtualizarTecnicos);*/
 		
 		JPanel panel_3 = new JPanel();
 		tabbedPane.addTab("Jogos", null, panel_3, null);
@@ -233,7 +252,7 @@ public class Interface extends JFrame {
 			}
 		});
 		btnNewButton_4.setAction(action);
-		btnNewButton_4.setBounds(252, 11, 111, 23);
+		btnNewButton_4.setBounds(253, 11, 111, 23);
 		panel_3.add(btnNewButton_4);
 		
 	}
