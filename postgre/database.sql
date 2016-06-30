1ced8076a2aabd2d4ed0a7c7ab0c10f8fbb4ef8c
tuliolima <tuliook@hotmail.com>
2016-06-30 09:09:35 -0300

    Arquivos do BD
    Estão na pasta postgre.

---
+++ postgre/database.sql
@@ -0,0 +1,142 @@
+drop schema public cascade;	-- Comando para apagar o banco de dados existente
+create schema public;		-- Criar o novo banco
+
+CREATE TABLE Rodada (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null
+);
+
+CREATE TABLE Juiz (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	idade int not null,
+	nacionalidade varchar(50) not null
+);
+
+CREATE TABLE Continente (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null
+);
+
+CREATE TABLE Cidade (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	estado varchar(50) not null
+);
+
+CREATE TABLE Estadio (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	max_torcedores int not null,
+	id_cidade int not null,
+	FOREIGN KEY(id_cidade) REFERENCES Cidade (id)
+);
+
+create table Centro_Olimpico (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	id_cidade int not null,
+	FOREIGN KEY(id_cidade) REFERENCES Cidade (id)
+);
+
+CREATE TABLE Selecao (
+	id serial PRIMARY KEY,
+	pais varchar(50) not null,
+	pontos int not null,
+	foto_equipe bytea not null,		-- campo para colocar a foto da equipe
+	id_continente int not null,
+	id_centro_olimpico int not null,
+	FOREIGN KEY(id_centro_olimpico) REFERENCES Centro_Olimpico (id),
+	FOREIGN KEY(id_continente) REFERENCES Continente (id)
+);
+
+CREATE TABLE Partida (
+	id serial PRIMARY KEY,
+	data_hora TIMESTAMP WITH TIME ZONE not null,
+	placarA int not null,
+	placarB int not null,
+	id_rodada int not null,
+	id_juiz int not null,
+	id_estadio int not null,
+	id_selecaoA int not null,
+	id_selecaoB int not null,
+	FOREIGN KEY(id_rodada) REFERENCES Rodada (id),
+	FOREIGN KEY(id_juiz) REFERENCES Juiz (id),
+	FOREIGN KEY(id_estadio) REFERENCES Estadio (id),
+	FOREIGN KEY(id_selecaoA) REFERENCES Selecao (id),
+	FOREIGN KEY(id_SelecaoB) REFERENCES Selecao (id)
+);
+
+CREATE TABLE Jogador (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	idade int not null,
+	nacionalidade varchar(50) not null,
+	id_selecao int not null,
+	FOREIGN KEY(id_selecao) REFERENCES Selecao (id)
+);
+
+CREATE TABLE Tecnico (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	idade int not null,
+	nacionalidade varchar(50) not null,
+	id_selecao int not null,
+	FOREIGN KEY(id_selecao) REFERENCES Selecao (id)
+);
+
+CREATE TABLE Patrocinador (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	id_selecao int not null,
+	FOREIGN KEY(id_selecao) REFERENCES Selecao (id)
+);
+
+CREATE TABLE Torcida (
+	id serial PRIMARY KEY,
+	nome varchar(50) not null,
+	n_torcedores int not null,
+	id_selecao int not null,
+	FOREIGN KEY(id_selecao) REFERENCES Selecao (id)
+);
+
+CREATE TABLE Premiacao (
+	id serial PRIMARY KEY,
+	categoria varchar(50) not null,
+	id_selecao int not null,
+	FOREIGN KEY(id_selecao) REFERENCES Selecao (id)
+);
+
+CREATE TABLE Titulo (
+	id serial PRIMARY KEY,
+	campeonato varchar(50) not null,
+	ano date not null,
+	id_selecao int not null,
+	FOREIGN KEY(id_selecao) REFERENCES Selecao (id)
+);
+
+create table Equipe_medica (
+	id serial PRIMARY KEY,
+	n_integrantes int not null,
+	id_selecao int not null,
+	FOREIGN KEY(id_selecao) REFERENCES Selecao (id)
+);
+
+CREATE VIEW tabela_de_jogos as
+	select 	rodada.nome as "Rodada",
+			selecaoA.pais as "Time A",
+			Partida.placarA as "A",
+			Partida.placarB as "B",
+			selecaoB.pais as "Time B",
+			estadio.nome as "Local",
+			Partida.data_hora as "Data/Hora"
+	from	rodada,
+			Partida,
+			Selecao selecaoA,
+			Selecao selecaoB,
+			estadio
+	where	rodada.id = Partida.id_rodada
+		and	partida.id_selecaoa = selecaoA.id
+		and	partida.id_selecaoB = selecaoB.id
+		and	partida.id_estadio = estadio.id;
+
---
+++ postgre/inserts.sql
@@ -0,0 +1,106 @@
+-- Inserts para o banco de dados de futebol das Olimpíadas Rio 2016
+
+insert into Cidade (nome, estado) values ('Sao Paulo', 'SP');
+insert into Cidade (nome, estado) values ('Manaus', 'AM');
+insert into Cidade (nome, estado) values ('Brasilia', 'DF');
+insert into Cidade (nome, estado) values ('Salvador', 'BA');
+insert into Cidade (nome, estado) values ('Rio de Janeiro', 'RJ');
+insert into Cidade (nome, estado) values ('Belo Horizonte', 'BH');
+
+insert into Estadio (nome, max_torcedores, id_cidade) values ('Arena Corinthians', 49205, 1);
+insert into Estadio (nome, max_torcedores, id_cidade) values ('Arena Amazonia', 40549, 2);
+insert into Estadio (nome, max_torcedores, id_cidade) values ('Arena Fonte Nova', 50000, 4);
+insert into Estadio (nome, max_torcedores, id_cidade) values ('Estadio Mane Garrincha', 72788, 3);
+insert into Estadio (nome, max_torcedores, id_cidade) values ('Estadio Olimpico', 46931, 5);
+insert into Estadio (nome, max_torcedores, id_cidade) values ('Maracana', 78838, 5);
+insert into Estadio (nome, max_torcedores, id_cidade) values ('Mineirao', 61846, 6);
+
+insert into Juiz (nome, idade, nacionalidade) values ('Adailton Souza Barros', 33, 'Brasileiro');
+insert into Juiz (nome, idade, nacionalidade) values ('Jaroslaw Kaczynski', 42, 'Polones');
+insert into Juiz (nome, idade, nacionalidade) values ('Enrique Pena Nieto', 35, 'Mexicano');
+
+insert into Continente (nome) values ('America');
+insert into Continente (nome) values ('Africa');
+insert into Continente (nome) values ('Europa');
+insert into Continente (nome) values ('Asia');
+insert into Continente (nome) values ('Oceania');
+insert into Continente (nome) values ('Antartida');
+
+insert into Rodada (nome) values ('Quartas de final');
+insert into Rodada (nome) values ('Semifinais');
+insert into Rodada (nome) values ('Disputa do Bronze');
+insert into Rodada (nome) values ('Final');
+
+insert into Centro_Olimpico (nome, id_cidade) values ('Pele', 1);
+insert into Centro_Olimpico (nome, id_cidade) values ('Dunga', 2); 
+insert into Centro_Olimpico (nome, id_cidade) values ('Mane Garrincha', 3); 
+insert into Centro_Olimpico (nome, id_cidade) values ('Chaves', 4); 
+insert into Centro_Olimpico (nome, id_cidade) values ('Vila Nova', 5); 
+
+insert into Selecao (pais, pontos, foto_equipe, id_centro_olimpico, id_continente) values('Brasil', 0, '0', 1, 1);
+insert into Selecao (pais, pontos, foto_equipe, id_centro_olimpico, id_continente) values('Estados Unidos', 0, '0', 2, 1);
+insert into Selecao (pais, pontos, foto_equipe, id_centro_olimpico, id_continente) values('Inglaterra', 0, '0', 3, 3);
+insert into Selecao (pais, pontos, foto_equipe, id_centro_olimpico, id_continente) values('Nigeria', 0, '0', 4, 2);
+insert into Selecao (pais, pontos, foto_equipe, id_centro_olimpico, id_continente) values('China', 0, '0', 5, 4);
+
+insert into Torcida (nome, n_torcedores, id_selecao) values ('Canarinho', 750, 1);
+insert into Torcida (nome, n_torcedores, id_selecao) values ('Screaming Eagles', 500, 2);
+insert into Torcida (nome, n_torcedores, id_selecao) values ('Hooligans', 372, 3);
+
+insert into Titulo (campeonato, ano, id_selecao) values ('Campeao Olimpico', '10-07-1996', 4);
+insert into Titulo (campeonato, ano, id_selecao) values ('Campeao Olimpico', '23-06-1912', 3);
+insert into Titulo (campeonato, ano, id_selecao) values ('Campeao do Mundo', '13-07-2002', 1);
+insert into Titulo (campeonato, ano, id_selecao) values ('Campeao do Mundo', '15-07-1994', 1);
+insert into Titulo (campeonato, ano, id_selecao) values ('Campeao do Mundo', '24-07-1970', 1);
+
+insert into Tecnico (nome, idade, nacionalidade, id_selecao) values('Tite', 55, 'Brasileiro', 1);
+insert into Tecnico (nome, idade, nacionalidade, id_selecao) values('Jurgen Klinsmann', 51, 'Alemao', 2);
+insert into Tecnico (nome, idade, nacionalidade, id_selecao) values('Roy Hodgson', 68, 'Ingles', 3);
+insert into Tecnico (nome, idade, nacionalidade, id_selecao) values('Daniel Amokachi', 43, 'Nigeriano', 4);
+insert into Tecnico (nome, idade, nacionalidade, id_selecao) values('Alain Perrin', 59, 'Frances', 5);
+
+insert into Equipe_medica (n_integrantes, id_selecao) values(10, 1);
+insert into Equipe_medica (n_integrantes, id_selecao) values(9, 2);
+insert into Equipe_medica (n_integrantes, id_selecao) values(11, 3);
+insert into Equipe_medica (n_integrantes, id_selecao) values(7, 4);
+insert into Equipe_medica (n_integrantes, id_selecao) values(15, 5);
+
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Neymar', 24, 'Brasileiro', 1);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Kaka', 34, 'Brasileiro', 1);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Wondolowski', 33, 'Americano', 2);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Wood', 24, 'Americano', 2);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Rooney', 30, 'Ingles', 3);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Vardy', 29, 'Ingles', 3);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Mikel', 29, 'Nigeriano', 4);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Enyeama', 33, 'Nigeriano', 4);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Zhi', 35, 'Chines', 5);
+insert into Jogador (nome, idade, nacionalidade, id_selecao) values('Lin', 30, 'Chines', 5);
+
+insert into Patrocinador (nome, id_selecao) values ('Adidas', 2);
+insert into Patrocinador (nome, id_selecao) values ('Katsumi', 5);
+insert into Patrocinador (nome, id_selecao) values ('Afreaka', 4);
+insert into Patrocinador (nome, id_selecao) values ('McLaren', 3);
+insert into Patrocinador (nome, id_selecao) values ('Bradesco', 1);
+
+insert into premiacao (categoria, id_selecao) values ('Ouro', 1);
+insert into premiacao (categoria, id_selecao) values ('Prata', 4);
+insert into premiacao (categoria, id_selecao) values ('Bronze', 5);
+insert into premiacao (categoria, id_selecao) values ('Artilheiro', 1);
+insert into premiacao (categoria, id_selecao) values ('Melhor Jogador', 2);
+
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('20-08-2016 20:00:00-03', 3, 2, 4, 1, 6, 1, 3);
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('19-08-2016 20:00:00-03', 4, 1, 3, 3, 7, 2, 4);
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('17-08-2016 20:00:00-03', 2, 3, 2, 1, 5, 2, 1);
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('16-08-2016 20:00:00-03', 1, 2, 2, 2, 4, 4, 3);
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('13-08-2016 20:00:00-03', 3, 4, 1, 2, 3, 2, 4);
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('07-08-2016 20:00:00-03', 5, 2, 1, 3, 2, 2, 5);
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('06-08-2016 20:00:00-03', 1, 0, 1, 1, 1, 1, 3);
+insert into partida (data_hora ,placarA ,placarB ,id_rodada ,id_juiz ,id_estadio ,id_selecaoA ,id_selecaoB)
+	values ('12-08-2016 20:00:00-03', 3, 2, 1, 3, 6, 3, 5);
